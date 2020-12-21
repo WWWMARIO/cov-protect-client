293236cfc87a928e46c2eb405732a616866a2c00
Mario <mariomicetic@gmail.com>
2020-12-21 21:40:59 +0100

    firebase

--- functions/index.js
+++ functions/index.js
@@ -1,20 +1,40 @@
-// const functions = require('firebase-functions');
-
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
-
-
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+var admin = require("firebase-admin");
+
+var serviceAccount = require("./cov-protect-firebase-adminsdk-wqt6p-fc18fdc883.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://cov-protect.firebaseio.com"
+});
+
+const db = admin.firestore();
+
+app.get('/stations', (request, response) => {
+      let res = [];
+      db.collection('stations')
+          .get()
+          .then((querySnapshot) => {
+              querySnapshot.forEach((doc) => {
+                  let document = {
+                      id: doc.id,
+                      data: doc.data()
+                  }
+                  res.push(document)
+              })
+              return response.send(res)
+          })
+          .catch ((error) => {
+              return response.send("Error getting documents: ", error);
+          })
+  })
+
 
 app.get('/hello', (request, response) => {
     return response.send('Hello world2');
@@ -38,3 +58,15 @@ app.post('/addStation', (request, response) => {
 app.listen(3000, () => {
     console.log("Server running on port 3000");
 });
+
+
+// const functions = require('firebase-functions');
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
+
